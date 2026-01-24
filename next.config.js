1e80f19a21e07d07538952bf3662b2b5af326bac
kadioko <godfreymariki@gmail.com>
2026-01-24 18:44:59 +0300

    Convert next.config.ts to next.config.js for Next.js 14.2.3 compatibility

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typedRoutes: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
